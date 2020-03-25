dbab5136b507f37d1202968297a924e450fd45b6
Ilya Kushlianski <kushliansky@gmail.com>
2020-03-26 01:01:36 +0300

    Update TS

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Alert from 'react-bootstrap/Alert';
 
 function App() {
   return (
     <div className="App">
-
+      <Alert variant="primary">something</Alert>
     </div>
   );
 }
