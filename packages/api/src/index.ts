98fdc131b5429027f8061099693914cfe739dc96
Ilya Kushlianski <kushliansky@gmail.com>
2020-03-28 02:32:25 +0300

    Set up eslint and prettier for api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,13 +1,13 @@
-import * as express from "express";
+import * as express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello efff")
+app.get('/', (req, res) => {
+  res.send('Hello efff');
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
