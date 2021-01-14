1b2c92426d151494c7ce92b4c804c1d34c931327
Ilya Kushlianski <kushliansky@gmail.com>
2021-01-14 20:10:00 +0300

    start creating user model

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { IterationModule } from './entities/iteration/iteration.module';
 import { DatabaseModule } from './database/database.module';
+import { OpportunityModule } from './entities/opportunity/opportunity.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), IterationModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    IterationModule,
+    DatabaseModule,
+    OpportunityModule,
+  ],
   controllers: [],
   providers: [],
 })
