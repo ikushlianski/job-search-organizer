02a56ba49edd1b0a4a6068cc5cd4370c7af4965b
Ilya Kushlianski <kushliansky@gmail.com>
2021-04-24 00:24:54 +0300

    add seeders for opportunity, project and opportunity-answer

---
+++ packages/api/src/database/seeders-dev/20210422220628-project.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+      insert into public.jso_project
+      (name) 
+      VALUES
+      ('Some test project 1'),
+      ('Test project 2')
+    `);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+      truncate public.jso_project RESTART IDENTITY CASCADE;
+    `);
+  },
+};
---
+++ packages/api/src/database/seeders-dev/20210423131611-opportunity.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+      insert into public.jso_opportunity 
+      (id, date, final_date, name, iteration_id, company_id, project_id, user_id) 
+      VALUES
+      (1, null, null, 'test opportunity', 1, 1, 1, 1)
+    `);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+      truncate public.jso_opportunity RESTART IDENTITY CASCADE;
+    `);
+  },
+};
